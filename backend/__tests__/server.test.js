e0b341ec05985b46f347654226fce1bb42174b37
Leonardo Beristain <beristain.lbc@gmail.com>
2025-04-07 14:09:01 -0300

    Actualizados archivos ignorados y cambios en backend tests si decidiste incluirlos

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const server = require('../server');
+const server = require('../beristain-contact-backend/server');
 
 afterAll(async () => {
   await server.close();
